40c8a6d0b32514e3d715d8fd5740aa2f38e830a4
bucricket <bucricket@gmail.com>
2017-02-09 11:55:12 -0500

    Fixed textfile

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ srcDir = os.path.join(processDir,'source','src')
 
 #=====write Makefile.local===========
 makeFilename = os.path.join(processDir,'source','build',"Makefile.local")
-print (makeFilename)
+
 file = open(makeFilename,"w") 
  
 file.write("HDF5_PREFIX  = %s" % prefix) 
-file.write("FFLAGS_HDF5  = -D_RTTOV_HDF $(FFLAG_MOD)$(HDF5_PREFIX)/include") 
-file.write("LDFLAGS_HDF5 = -L$(HDF5_PREFIX)/lib -lhdf5hl_fortran -lhdf5_hl -lhdf5_fortran -lhdf5") 
-file.write("FFLAGS_EXTERN  = $(FFLAGS_NETCDF)  $(FFLAGS_HDF5)  $(FFLAGS_DRHOOK)") 
+file.write("FFLAGS_HDF5  = -D_RTTOV_HDF $(FFLAG_MOD)$(HDF5_PREFIX)/include\n") 
+file.write("LDFLAGS_HDF5 = -L$(HDF5_PREFIX)/lib -lhdf5hl_fortran -lhdf5_hl -lhdf5_fortran -lhdf5\n") 
+file.write("FFLAGS_EXTERN  = $(FFLAGS_NETCDF)  $(FFLAGS_HDF5)  $(FFLAGS_DRHOOK)\n") 
 file.write("LDFLAGS_EXTERN = $(LDFLAGS_NETCDF) $(LDFLAGS_HDF5) $(LDFLAGS_DRHOOK)")
 file.close() 
 
