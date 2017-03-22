f5b210ac564226f6fa3667464a2b3ca9373ed595
bucricket <bucricket@gmail.com>
2017-03-22 08:59:45 -0400

    Trying to adhere to standards

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ import subprocess
 import os
 import shutil
 import tarfile
+from setuptools import setup
 
 
 
 def untar(fname, fpath):
-    if (fname.endswith('tar.gz') or fname.endswith('tar.bz') or fname.endswith('tar')):
+    if fname.endswith('tar.gz') or fname.endswith('tar.bz') or fname.endswith('tar'):
         tar = tarfile.open(fname)
-        tar.extractall(path = fpath)
+        tar.extractall(path=fpath)
         tar.close()
         os.remove(fname)
-        
 base = os.getcwd()
-prefix  = os.environ.get('PREFIX')
+prefix = os.environ.get('PREFIX')
 processDi = os.path.abspath(os.path.join(prefix,os.pardir))
 processDir = os.path.join(processDi,'work')
 binDir = os.path.join(prefix,'bin')
@@ -24,14 +24,14 @@ srcDir = os.path.join(processDir,'source','src')
 #=====write Makefile.local===========
 makeFilename = os.path.join(processDir,'source','build',"Makefile.local")
 
-file = open(makeFilename,"w") 
- 
-file.write("HDF5_PREFIX  = %s\n" % prefix) 
-file.write("FFLAGS_HDF5  = -D_RTTOV_HDF $(FFLAG_MOD)$(HDF5_PREFIX)/include\n") 
-file.write("LDFLAGS_HDF5 = -L$(HDF5_PREFIX)/lib -lhdf5hl_fortran -lhdf5_hl -lhdf5_fortran -lhdf5\n") 
-file.write("FFLAGS_EXTERN  = $(FFLAGS_NETCDF)  $(FFLAGS_HDF5)  $(FFLAGS_DRHOOK)\n") 
-file.write("LDFLAGS_EXTERN = $(LDFLAGS_NETCDF) $(LDFLAGS_HDF5) $(LDFLAGS_DRHOOK)")
-file.close() 
+fn = open(makeFilename,"w")
+
+fn.write("HDF5_PREFIX  = %s\n" % prefix)
+fn.write("FFLAGS_HDF5  = -D_RTTOV_HDF $(FFLAG_MOD)$(HDF5_PREFIX)/include\n")
+fn.write("LDFLAGS_HDF5 = -L$(HDF5_PREFIX)/lib -lhdf5hl_fortran -lhdf5_hl -lhdf5_fortran -lhdf5\n")
+fn.write("FFLAGS_EXTERN  = $(FFLAGS_NETCDF)  $(FFLAGS_HDF5)  $(FFLAGS_DRHOOK)\n")
+fn.write("LDFLAGS_EXTERN = $(LDFLAGS_NETCDF) $(LDFLAGS_HDF5) $(LDFLAGS_DRHOOK)")
+fn.close()
 
 
 #=====compile rttov=================
@@ -52,17 +52,13 @@ condaPath = out[0][:-1]
 
 os.chdir(base)
 
-shutil.copyfile(os.path.join(libDir,'rttov_wrapper_f2py.so'),os.path.join(prefix,'lib','python2.7','site-packages','rttov_wrapper_f2py.so'))
+shutil.copyfile(os.path.join(libDir,'rttov_wrapper_f2py.so'),
+                os.path.join(prefix,'lib','python2.7',
+                             'site-packages','rttov_wrapper_f2py.so'))
 
-shutil.copyfile(os.path.join(processDir,'source','rtcoef_rttov11','rttov7pred54L','rtcoef_landsat_8_tirs.dat'),os.path.join(rttovPath,'rtcoef_landsat_8_tirs.dat'))
-#try:
-from setuptools import setup
-#    setup_kwargs = {'entry_points': {'console_scripts':['pyrttov=pyrttov.__init__:Rttov']}}
-#except ImportError:
-#    from distutils.core import setup
-#    setup_kwargs = {'scripts': ['bin/pyrttov']}
-#    
-#from pyrttov import __version__
+shutil.copyfile(os.path.join(processDir,'source','rtcoef_rttov11',
+                             'rttov7pred54L','rtcoef_landsat_8_tirs.dat'),
+                os.path.join(rttovPath,'rtcoef_landsat_8_tirs.dat'))
 
 setup(
     name="pyrttov",
@@ -70,7 +66,7 @@ setup(
     description="pythonic wrapper for rttov",
     author="Mitchell Schull",
     author_email="mitch.schull@noaa.gov",
-    packages= ['pyrttov'],
+    packages=['pyrttov'],
     platforms='Posix; MacOS X; Windows',
     license='BSD 3-Clause',
     classifiers=[
@@ -82,7 +78,6 @@ setup(
         # Uses dictionary comprehensions ==> 2.7 only
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: GIS',
-    ],  
-#    **setup_kwargs
+    ],
 )
 
